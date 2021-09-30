727779ebbd064cdc4b30e5db851e4c9b33a2e2e6
boxnaster <nastya3362@yandex.ru>
2021-09-30 22:07:10 +0400

    Initialized the project

---
+++ RandomCatsApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RandomCatsApp
+//
+//  Created by Анастасия Тимофеева on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
